455e04d1c8c01d391e3f00caaac47a8fdd4da4fc
emuller1996 <stefano.muller@hotmail.com>
2023-01-04 21:52:05 -0500

    feat(Dasboard/Barbers) add toast and create barber

--- src/components/Dashboard/Barbers/Barbers.jsx
+++ src/components/Dashboard/Barbers/Barbers.jsx
@@ -1,36 +1,34 @@
 import React from 'react';
-
+import { ToastContainer } from 'react-toastify';
+import BarbersList from './BarbersList';
+import FormBarber from './FormBarber';
 
 export default function Barbers() {
 
 
     return (
-        <>
+        <div className='p-2'>
             <div className="row">
                 <div className="col-md-3 col-xl-2 col-5  ">
-                    <button type="button" class="btn btn-secondary w-100">Nuevo Barbero</button>
+                    <button data-toggle="modal" data-target="#modalBaber" type="button" class="btn btn-secondary w-100 rounded-0">Nuevo Barbero</button>
                 </div>
                 <div className="col-12 col-md-6">
-                    <input type="text" name="barberNameSearch" id="barberNameSearch" className="form-control rounded-0"  />
+                    <input type="text" name="barberNameSearch" id="barberNameSearch" className="form-control rounded-0" />
                 </div>
             </div>
 
-            <div className="row mt-4">
-                <div className="col-12 col-md-6 col-xl-4">
-                    <div className="card rounded-0 border-secondary card-barber p-1">
-                        <div className="row">
-                            <div className="col-6">
-                                <img src="/img/team-1.jpg" alt="ssss" className="img-fluid"/>
-                            </div>
-                            <div className="col-6">
-                                <p>Estefano </p>
-                            </div>
-                        </div>
+            <BarbersList />
 
+
+            <div class="modal  fade" id="modalBaber" tabindex="-1" aria-labelledby="modalBaber" aria-hidden="true">
+                <div class="modal-dialog modal-lg">
+                    <div class="modal-content bg-cafe rounded-0">
+                        <FormBarber />
+                        
                     </div>
                 </div>
             </div>
-
-        </>
+            
+        </div>
     )
 }

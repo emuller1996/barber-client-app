2e6b4b04de56510f138e811795fd11c386305844
emuller1996 <stefano.muller@hotmail.com>
2023-04-01 22:52:44 -0500

    feat(BarberDetail) se añadio actualizar barbero con sus respetivos servicios

--- src/components/Dashboard/Barbers/BarberDetail.jsx
+++ src/components/Dashboard/Barbers/BarberDetail.jsx
@@ -1,72 +1,170 @@
-import axios from 'axios';
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-
-
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useLocalStorage } from "../../../hooks/useLocalStorage";
 
 export default function BarberDetail() {
-    const params = useParams();
-    const history = useHistory();
-    const [barber, setBarber] = useState();
+  const params = useParams();
+  const history = useHistory();
+  const [barber, setBarber] = useState(undefined);
+  const [services, setServices] = useState([]);
+  const [servicesSelected, setServicesSelected] = useState([]);
+  const [token, setToken] = useLocalStorage("token", undefined);
 
-    useEffect(() => {
-        getBarber()
-    }, [])
+  useEffect(() => {
+    getBarber();
+    getServices();
+  }, []);
 
-
-    const getBarber = async () => {
-
-        try {
-
-            const result = await axios.get(`/barber/${params.idBarber}`);
-            console.log(result.data.barber);
-            setBarber(result.data.barber);
-
-        } catch (error) {
-            toast.error(error.message)
-        }
-
-
-
-
+  const handleInputBarber = (e) => {
+    setBarber({ ...barber, [e.target.name]: e.target.value });
+  };
+  const getBarber = async () => {
+    try {
+      const result = await axios.get(`/barber/${params.idBarber}`);
+      console.log(result.data.barber);
+      setBarber(result.data.barber);
+      setServicesSelected(result.data.barber.services.map((s) => s._id));
+    } catch (error) {
+      toast.error(error.message);
     }
+  };
 
-    return (
-        <div className="container bg-light p-3">
-            <button onClick={() => { history.goBack() }} type="button" class="close float-right py-1" aria-label="Close">
-                <i class="fas fa-arrow-left"></i>
-            </button>
-            <h4> {barber ? barber.name : ' . . .'}</h4>
+  const getServices = async () => {
+    setServices(undefined);
 
-            <div className="row">
-                <div className="col-6">
-                    <div class="row ">
-                        <div class="form-group col-12">
-                            <label for="nameBarber">Nombre barbero</label>
-                            <input type="text" class="form-control input-appointment" name="name" value={barber ? barber.name : ''}  id="nameBarber" />
-                            
-                        </div>
-                        <div class="form-group col-12">
-                            <label for="nameBarber">Rol barbero</label>
-                            <input type="text" class="form-control input-appointment" name="name" value={barber ? barber.role : ''}  id="nameBarber" />
-                            
-                        </div>
-                        <div class="form-group col-12">
-                            <label for="nameBarber">imagen barbero</label>
-                            <input type="text" class="form-control input-appointment" name="name" value={barber ? barber.image : ''}  id="nameBarber" />
-                            
-                        </div>
-                        
-                        
+    const result = await axios.get(`/services/`);
+    setServices(result.data.services);
+    console.log(result.data.services);
+  };
 
-                    </div>
+  const handleInputServicesAppointment = (e) => {
+    console.log("handleInputServicesAppointment");
+    console.log(e.target.value);
+    const servicesIn = servicesSelected.find((t) => t === e.target.value);
+    if (servicesIn) {
+      setServicesSelected(servicesSelected.filter((t) => t !== e.target.value));
+    } else {
+      setServicesSelected([...servicesSelected, e.target.value]);
+    }
+  };
 
-                </div>
+  const updateBarber = async () => {
+    delete barber.services;
+    /* console.log(barber);
+    console.log(servicesSelected); */
+    console.log(Object.assign(barber, { services: servicesSelected }));
+
+    try {
+      const result = await axios.put(
+        `/barber/${barber._id}`,
+        Object.assign(barber, { services: servicesSelected }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": token,
+          },
+        }
+      );
+      toast.success(result.data.message);
+      console.log(result);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="container bg-light p-3">
+      <button
+        onClick={() => {
+          history.goBack();
+        }}
+        type="button"
+        class="close float-right py-1"
+        aria-label="Close"
+      >
+        <i class="fas fa-arrow-left"></i>
+      </button>
+      <h4> {barber ? barber.name : " . . ."}</h4>
+
+      <div className="row">
+        <div className="col-md-6">
+          <div class="row ">
+            <div class="form-group col-12">
+              <label for="nameBarber">Nombre barbero</label>
+              <input
+                type="text"
+                class="form-control input-appointment"
+                name="name"
+                value={barber ? barber.name : ""}
+                id="nameBarber"
+                onChange={handleInputBarber}
+              />
             </div>
+            <div class="form-group col-12">
+              <label for="nameBarber">Rol barbero</label>
+              <input
+                type="text"
+                class="form-control input-appointment"
+                name="role"
+                value={barber ? barber.role : ""}
+                onChange={handleInputBarber}
+                id="nameBarber"
+              />
+            </div>
+            <div class="form-group col-12">
+              <label for="nameBarber">imagen barbero</label>
+              <input
+                type="text"
+                class="form-control input-appointment"
+                name="name"
+                value={barber ? barber.image : ""}
+                id="nameBarber"
+              />
+            </div>
+          </div>
         </div>
-    )
 
+        <div className="col-md-6">
+          <div className="row">
+            {services &&
+              services.length > 0 &&
+              services.map((s) => (
+                <div className="col-md-6 col-xl-4">
+                  <label className="w-100" htmlFor={s.name}>
+                    <div className="card w-100 rounded-0">
+                      <div class="card-header bg-light">
+                        <input
+                          checked={barber && servicesSelected.includes(s._id)}
+                          onChange={handleInputServicesAppointment}
+                          className="float-right mt-1"
+                          type="checkbox"
+                          name="services"
+                          id={s.name}
+                          value={s._id}
+                        />
+                        <span>{s.name}</span>
+                      </div>
+                    </div>
+                  </label>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        <div className="col-12">
+          <button
+            type="button"
+            onClick={updateBarber}
+            class="btn btn-primary w-50"
+          >
+            Actualizar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,75 +1,93 @@
-import axios from 'axios';
-import React,{useState} from 'react';
-import { useHistory } from 'react-router-dom';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 export default function FormLogin() {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState(undefined);
 
+  const history = useHistory();
+  const [token, setToken] = useLocalStorage("token", undefined);
 
-    const [input, setInput] = useState('');
-    const history = useHistory();
-    const [token, setToken] = useLocalStorage("token", undefined);
+  const handleInput = function (e) {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
 
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    console.log("handleSumbit");
+    console.log(input);
+    setError(undefined);
+    try {
+      const result = await axios.post("/auth/signin", input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log(result.data);
+      setToken(result.data.token);
+      history.push("/Dashboard/");
+      console.log(token);
+    } catch (error) {
+      console.log(error);
 
-    const handleInput = function (e) {
-        setInput( {...input , [e.target.name] : e.target.value} );
+      setError(error.message);
+    }
+  };
 
-      };
+  return (
+    <>
+      <form
+        action=""
+        className=" p-4"
+        autoComplete="off"
+        onSubmit={handleSumbit}
+      >
+        <div className="row justify-content-md-center">
+          <div className="col-md-8">
+            <div className="login">
+              <p>Ingrese sus credenciales</p>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control input-appointment"
+                  onChange={handleInput}
+                  value={input.email}
+                  id="email"
+                  name="email"
+                  placeholder="Correo"
+                />
+              </div>
 
-      const handleSumbit = async (e)=>{
-        e.preventDefault();
-        console.log("handleSumbit")
-        console.log(input)
-        try {
-            const result = await axios.post('/auth/signin',input,{
-                headers: {
-                'Content-Type': 'application/json'
-                }
-            });
-        console.log(result.data)
-        setToken(result.data.token)
-        history.push('/Dashboard/')
-        console.log(token)
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control input-appointment"
+                  onChange={handleInput}
+                  value={input.password}
+                  id="password"
+                  name="password"
+                  placeholder="Contraseña"
+                />
+              </div>
 
-
-        } catch (error) {
-        console.log(error)
-        console.log(error.message)
-        }
-        
-
-
-
-        
-      }
-
-
-
-    return (
-        <>
-            <form action="" className=' p-4' autoComplete='off' onSubmit={handleSumbit}>
-                <div className="row justify-content-md-center">
-                    <div className="col-md-8">
-                        <div className="login">
-                            <p>Ingrese sus credenciales</p>
-                            <div class="form-group">
-                                <input type="email" class="form-control input-appointment"  onChange={handleInput} value={input.email} id="email" name="email" placeholder="Correo" />
-                            </div>
-
-                            <div class="form-group">
-                                <input type="password" class="form-control input-appointment"  onChange={handleInput}  value={input.password} id="password" name="password" placeholder="Contraseña" />
-                            </div>
-
-                            <button type="submit" class="btn btn-dark rounded-0">Submit</button>
-
-
-                        </div>
-
-                    </div>
-
+              {error && (
+                <div
+                  class="alert alert-danger rounded-0 shadow-md"
+                  role="alert"
+                >
+                  <strong>{error}</strong>
                 </div>
-            </form>
-        </>
-    )
+              )}
+
+              <button type="submit" class="btn btn-dark rounded-0">
+                Ingresar
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }

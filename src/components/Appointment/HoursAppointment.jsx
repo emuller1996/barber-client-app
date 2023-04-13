95fe3bf49740f774fe39c43d6946bc084ab2f434
emuller1996 <stefano.muller@hotmail.com>
2023-04-12 23:57:54 -0500

    fix (Appointment) se arreglos y se añadio estilos

--- src/components/Appointment/HoursAppointment.jsx
+++ src/components/Appointment/HoursAppointment.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function HoursAppointment({ hoursAvailable,setHours,dateAppointment, setDateAppointment }) {
+export default function HoursAppointment({ hoursAvailable,setHours,dateAppointment, setDateAppointment, hours }) {
 
 
   if (!hoursAvailable) 
@@ -19,16 +19,16 @@ export default function HoursAppointment({ hoursAvailable,setHours,dateAppointme
       </div>
     );
   return (
-    <div className="row mt-2 align-items-center">
+    <div className="row mx-1 mt-2 align-items-center">
       
-
       {hoursAvailable.map((h) => (
-        <div className="col-4 col-md-4 col-xl-3  mb-2">
+        <div key={h.hour} className="col-4 col-md-4 col-xl-3  mb-2">
+          
           <div
-            className="border border-white bg-dark text-white shadow-sm rounded p-2"
-          ><label htmlFor={h.hour} className="w-100 h-100 m-0">
-            <input type="radio" name="hour" value={h.hour} id={h.hour} onClick={(e) => { setHours(`${e.target.value}`) }} />
-            <span> {h.hour} </span>
+            className={hours===h.hour ? "card-service-selected " : "card-service "}
+          ><label htmlFor={h.hour} className="w-100 h-100 m-0 p-2">
+            <input  type="radio" name="hour" value={h.hour} id={h.hour} onClick={(e) => { setHours(`${e.target.value}`) }} />
+            <span> {h.view} </span>
           </label>
             
           </div>
